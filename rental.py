3bd4961178729e1999d47638c0dcb4dc7154afc9
Enchooo <obamacarenicho@.com>
2025-08-28 16:36:11 +0700

    final rental apk(maybe)

--- rental.py
+++ rental.py
@@ -1,5 +1,53 @@
-pilihan = 'y'
-while pilihan == 'y':
+import uuid
+import json
+
+FILE = "rental.json"
+
+try:
+    with open(FILE, "r") as file: # Buka file JSON, "r" = read
+        data = json.load(file) # Baca isi file JSON, lalu ubah (load) jadi objek Python.
+except FileNotFoundError:
+    data = []
+
+print("""
+======================================================
+      /nSelamat Datang di Rental Kendaraan Warjoeg
+=======================================================
+      """)
+    
+pertanyaan = str(input(" rental (y) atau kembalikan kendaraan(n)? (y/n): "))
+
+if pertanyaan == 'n':
+    print("Rental Kendaraan Warjoeg")
+    id = str(input("Masukkan id Kendaraan: "))
+    
+    transaksi = None
+    
+    for t in data:
+        if t["id"] == id:
+            transaksi = t
+            break
+        else :
+            transaksi = None
+            print("ID Kendaraan tidak ditemukan")
+    
+    if transaksi:
+        data.remove(transaksi)
+        
+        with open(FILE, "w") as file:
+            json.dump(data, file)
+            
+        print("\n✅ Pengembalian berhasil dicatat!")
+        print("Terimakasih telah meminjam kendaraan")
+        
+    else:
+        print("ID Kendaraan tidak ditemukan")
+    
+   
+    
+else:
+    pertanyaan == 'y'
+
     print("""
     =======================================
          *** Rental Kendaraan Warjoeg ***
@@ -18,14 +66,28 @@ while pilihan == 'y':
     =======================================
     """)
 
-    nama = input("Nama Penyewa : ")
-    alamat = input("Alamat       : ")
-    ktp = input("No. KTP      : ")
-
-    jaminan = input("Jaminan (SIM/KTP/KK): ")
-
+    nama = str(input("Nama Penyewa : "))
+    alamat = str(input("Alamat       : "))
+    nohp = int(input("No. HP       : "))
+    ktp = int(input("No. KTP      : "))
+    unique_id = str(uuid.uuid4())[:8]  # ambil 8 karakter pertama biar ga kepanjangan
     kendaraan = input("Pilih kendaraan (a-h): ")
     lama = int(input("Lama pinjam (hari): "))
+    
+    penyewa = {
+        "id": unique_id,
+        "nama": nama,
+        "alamat": alamat,
+        "nohp": nohp,
+        "ktp": ktp,
+        "kendaraan": kendaraan,
+        "lama": lama
+    }
+    
+    data.append(penyewa)
+    
+    with open(FILE, "w") as file:
+        json.dump(data, file)
 
     if kendaraan == 'a':
         jenis = "Avanza"
@@ -53,17 +115,16 @@ while pilihan == 'y':
         harga = 160000
     else:
         print("Pilihan tidak tersedia!")
-        continue
+        exit()
 
     subtotal = harga * lama
 
-    if lama > 7:
+    if lama >= 5:
         diskon = int(subtotal * 0.1)  # diskon 10%
     else:
         diskon = 0
 
-    ppn = int((subtotal - diskon) * 0.1)
-    total = subtotal - diskon + ppn
+    total = subtotal - diskon
 
     print(f"\nTotal yang harus dibayar: Rp {total}")
     bayar = int(input("Masukkan nominal pembayaran: "))
@@ -74,14 +135,13 @@ while pilihan == 'y':
     print("Nama       :", nama)
     print("Alamat     :", alamat)
     print("No KTP     :", ktp)
-    print("Jaminan    :", jaminan)
     print("-----------------------------------")
     print("Kendaraan  :", jenis)
+    print("ID Kendaraan:", unique_id)
     print("Harga/Hari : Rp", harga)
     print("Lama Pinjam:", lama, "hari")
     print("Subtotal   : Rp", subtotal)
     print("Diskon     : Rp", diskon)
-    print("PPN (10%)  : Rp", ppn)
     print("-----------------------------------")
     print("TOTAL BAYAR: Rp", total)
     print("Dibayar    : Rp", bayar)
@@ -89,7 +149,9 @@ while pilihan == 'y':
     if bayar >= total:
         kembali = bayar - total
         print("Kembalian  : Rp", kembali)
+        print("Silahkan ambil kendaraan di tempat ")
         print("Status     : LUNAS ✅, Terimakasih")
+        print("Simpan id untuk pengembalian kendaraan")
     else:
         kurang = total - bayar
         print("Kekurangan : Rp", kurang)
@@ -97,4 +159,5 @@ while pilihan == 'y':
 
     print("===================================")
 
-    pilihan = input("\nApakah ingin rental lagi? (y/n): ")
+   
+    
